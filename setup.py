d97760628de494d6b41327db2e1f93583cbab455
teddy <teddy_chou@outlook.com>
2022-11-19 18:18:33 +0800

    the integration of datas

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='enzymepy',
+      version='1.0',
+      description='Python Tool For Enzyme Processing',
+      author='Hantao Zhou',
+      author_email='teddy_cou@outlook.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['enzymepy'],
+      package_dir = {'enzymepy': 'src/enzymepy'},
+      package_data = {'enzymepy':['data/BrendaIDwithCid_duplicated.pkl', 'data/syn.pkl']},
+      requires=['nltk', 'tqdm', 'rdkit', 'pubchempy'],
+      include_package_data = True,
+     )
